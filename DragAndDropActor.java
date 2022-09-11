7a0a942248a10e4af08b6e96669946b032f08fc8
Lukaszwutkowski <64429535+Lukaszwutkowski@users.noreply.github.com>
2022-09-11 12:27:22 +0200

    Drag and drop class and functionallity added

--- DragAndDropActor.java
+++ DragAndDropActor.java
@@ -1,6 +1,9 @@
+import com.badlogic.gdx.math.Interpolation;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 
 /**
  * Enables drag-and-drop functionality for actors.
@@ -8,23 +11,39 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
 public class DragAndDropActor extends BaseActor{
 
    private DragAndDropActor self;
+   private DropTargetActor dropTarget;
+   private boolean draggable;
 
    private float grabOffsetX;
    private float grabOffsetY;
 
+   private float startPositionX;
+   private float startPositionY;
+
 
     public DragAndDropActor(float x, float y, Stage s) {
         super(x, y, s);
+        draggable = true;
 
         self = this;
 
         addListener(
                 new InputListener(){
                     public boolean touchDown(InputEvent event, float offsetX, float offsetY, int pointer, int button){
+                        if (!self.isDraggable()){
+                            return false;
+                        }
+
                         self.grabOffsetX = offsetX;
                         self.grabOffsetY = offsetY;
 
+                        self.startPositionX = self.getX();
+                        self.startPositionY = self.getY();
+
+                        self.addAction(Actions.scaleTo(1.1f, 1.1f, 0.25f));
+
                         self.toFront();
+                        self.onDragStart();
 
                         return true;
                     }
@@ -37,7 +56,27 @@ public class DragAndDropActor extends BaseActor{
                     }
 
                     public void touchUp(InputEvent event, float offsetX, float offsetY, int pointer, int button){
-                        // add code later
+                        self.setDropTarget(null);
+
+                        // keep track of distance to the closest object
+                        float closestDistance = Float.MAX_VALUE;
+
+                        for (BaseActor actor : BaseActor.getList(self.getStage(), "DropTargetActor")){
+                            DropTargetActor target = (DropTargetActor)actor;
+
+                            if (target.isTargetable() && self.overlaps(target)){
+                                float currentDistance = Vector2.dst(self.getX(), self.getY(), target.getX(), target.getY());
+
+                                // check if this target is even closer
+                                if (currentDistance < closestDistance){
+                                    self.setDropTarget(target);
+                                    closestDistance = currentDistance;
+                                }
+                            }
+                        }
+
+                        self.addAction(Actions.scaleTo(1.00f, 1.00f, 0.25f));
+                        self.onDrop();
                     }
                 }
         );
@@ -45,5 +84,43 @@ public class DragAndDropActor extends BaseActor{
 
     public void act(float dt){
         super.act(dt);
+    }
+
+    public void setDraggable(boolean d){
+        draggable = d;
+    }
+
+    public  boolean isDraggable(){
+        return draggable;
+    }
+
+    public boolean hasDropTarget(){
+        return (dropTarget != null);
+    }
+
+    public void setDropTarget(DropTargetActor dt){
+        dropTarget = dt;
+    }
+
+    public DropTargetActor getDropTarget(){
+        return dropTarget;
+    }
+
+    public void moveToActor(BaseActor other){
+        float x = other.getX() + (other.getWidth() - this.getWidth()) / 2;
+        float y = other.getY() + (other.getHeight() - this.getHeight()) / 2;
+        addAction(Actions.moveTo(x, y, 0.50f, Interpolation.pow3));
+    }
+
+    public void moveToStart(){
+        addAction(Actions.moveTo(startPositionX, startPositionY, 0.50f, Interpolation.pow3));
+    }
+
+    public void onDragStart(){
+
+    }
+
+    public void onDrop(){
+
     }
 }
